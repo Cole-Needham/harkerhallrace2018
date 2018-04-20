7d9c5a64ba72bd54890439bac1f331cce52129ca
Cole-Needham <36093371+Cole-Needham@users.noreply.github.com>
2018-04-19 21:09:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, request
 from twilio import twiml
-
-app=Flask(__name__)
-
+ 
+ 
+app = Flask(__name__)
+ 
+ 
 @app.route('/sms', methods=['POST'])
 def sms():
-    number=request.form['From']
-    message_body=request.form['Body']
-    resp=twiml.Response()
-    resp.message('Hello Bitch, you said: {}'.format(number,message_body))
+    number = request.form['From']
+    message_body = request.form['Body']
+ 
+    resp = twiml.Response()
+    resp.message('Hello {}, you said: {}'.format(number, message_body))
     return str(resp)
-
-if __name__=='__main__':
+ 
+if __name__ == '__main__':
     app.run()
