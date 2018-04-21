b2a5a91218a894cc51b30495ac9aa59d744764f3
Cole Needham <coldlock720@gmail.com>
2018-04-20 17:16:49 -0700

    dank boy

--- app.py
+++ app.py
@@ -10,18 +10,21 @@ app = Flask(__name__)
 account_sid = "AC15b4f279248b559ac41e6e92f039ce30"
 auth_token = "b546ebf94dd2010a5646c4a68aadeac5"
 t_client = Client(account_sid,auth_token)
-@app.route("/",methods=['GET','POST'])
-@app.route("/app",methods=['GET','POST'])
-@app.route("/app/sms", methods=['GET', 'POST'])
-def sms():
-    """Respond to incoming calls with a simple text message."""
-    # Start our TwiML response
-    number = request.form['From']
-    message_body = request.form['Body']
-    resp = twiml.Response()
-    resp.message('Hello {}, you said: {}'.format(number, message_body))
-    return str(resp)
+@app.route("/sms/", methods=['GET', 'POST'])
+def sms_echo():
+    # https://github.com/twilio/twilio-python#api-credentials
 
+    sms_body = request.args.get('Body')
+    app.logger.info('body: `{}`'.format(sms_body))
+
+    if sms_body is None:
+        app.logger.warning('Null SMS body.')
+        return 'Bad request from Twilio client.'
+    else:
+        return ('<?xml version="1.0" encoding="UTF-8" ?>'
+                '<Response>'
+                '<Message>{}</Message>'
+                '</Response>'.format(sms_body))
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int('5000'))
 
